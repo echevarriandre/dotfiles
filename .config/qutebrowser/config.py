4ba02718e02d59f9299490559ca9d933df1cb855
André Echevarria <echevarriandre@gmail.com>
2020-07-26 00:03:18 +0100

    add dracula theme qutebrowser

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,12 @@
+# https://draculatheme.com/qutebrowser/
+import dracula.draw
+
+# Load existing settings made via :set
+config.load_autoconfig()
+
+dracula.draw.blood(c, {
+    'spacing': {
+        'vertical': 6,
+        'horizontal': 8
+    }
+})
